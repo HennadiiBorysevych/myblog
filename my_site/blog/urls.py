15aab9ccbd359de45bf7c1b2bf5f642d9ad63852
Hennadii Borysevych <genuch3@gmail.com>
2024-01-01 16:44:56 +0100

    feat:comment form and image upload

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.starting_page, name="starting-page" ),
-    path('posts', views.posts, name="posts-page"),
-    path('posts/<slug:slug>', views.post_detaild, name="posts-detail-page" ), #
+    path('',views.StartingPageView.as_view(), name="starting-page" ),
+    path('posts', views.AllPostsView.as_view(), name="posts-page"),
+    path('posts/<slug:slug>', views.SinglePostView.as_view(), name="posts-detail-page" ), #
 ]
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,9 +1,16 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from django.views import View
 from django.http import HttpResponse
 from datetime import date
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 from .models import Post
+from .forms import CommentForm
 # Create your views here.
 
 # all_posts = [
@@ -77,23 +84,72 @@ def get_date(post):
     return post['date']
 
 
-def starting_page(req):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    # sorted_posts = sorted(all_posts, key=get_date)  # ????
+class StartingPageView(ListView):
+    template_name = 'blog/index.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'posts'
 
-    return render(req, 'blog/index.html', {"posts": latest_posts})
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+# def starting_page(req):
+#     latest_posts = Post.objects.all().order_by("-date")[:3]
+#     # sorted_posts = sorted(all_posts, key=get_date)  # ????
+
+#     return render(req, 'blog/index.html', {"posts": latest_posts})
 
 
-def posts(req):
-    all_posts = Post.objects.all().order_by("-date")
+class AllPostsView(ListView):
+    template_name = 'blog/index.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'all_posts'
 
-    return render(req, 'blog/all-posts.html', {"all_posts": all_posts})
+# def posts(req):
+#     all_posts = Post.objects.all().order_by("-date")
+
+#     return render(req, 'blog/all-posts.html', {"all_posts": all_posts})
 
 
-def post_detaild(req, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
+class SinglePostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'comment_form': CommentForm()
+        }
 
-    # selected_post = Post.objects.get(slug=slug)
+        return render(request, 'blog/post-detail.html', context)
 
-    # selected_post = next(post for post in all_posts if post['slug'] == slug)
-    return render(req, 'blog/post-detail.html', {"post": identified_post,"post_tags": identified_post.tags.all()})
+    def post(self, request,slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse('posts-detail-page',args=[slug]))
+        
+        context = {
+            'post': post,
+            'post_tags': post.tags.all(),
+            'comment_form': CommentForm()
+        }
+
+        return render(request, 'blog/post-detail.html', context)
+        
+
+
+# def post_detaild(req, slug):
+#     identified_post = get_object_or_404(Post, slug=slug)
+
+#     # selected_post = Post.objects.get(slug=slug)
+
+#     # selected_post = next(post for post in all_posts if post['slug'] == slug)
+#     return render(req, 'blog/post-detail.html', {"post": identified_post, "post_tags": identified_post.tags.all()})
