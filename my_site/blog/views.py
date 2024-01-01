8e491f91f13c5a4c81257952893b5a28388853cc
Hennadii Borysevych <genuch3@gmail.com>
2024-01-01 17:28:27 +0100

    feat:comment section and errors handiling

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -120,12 +120,13 @@ class SinglePostView(View):
         context = {
             'post': post,
             'post_tags': post.tags.all(),
-            'comment_form': CommentForm()
+            'comment_form': CommentForm(),
+            'comments': post.comments.all().order_by('-id'),
         }
 
         return render(request, 'blog/post-detail.html', context)
 
-    def post(self, request,slug):
+    def post(self, request, slug):
         comment_form = CommentForm(request.POST)
         post = Post.objects.get(slug=slug)
 
@@ -134,16 +135,17 @@ class SinglePostView(View):
             comment.post = post
             comment.save()
 
-            return HttpResponseRedirect(reverse('posts-detail-page',args=[slug]))
-        
+            return HttpResponseRedirect(reverse('post-detail-page', args=[slug]))
+
         context = {
             'post': post,
             'post_tags': post.tags.all(),
-            'comment_form': CommentForm()
+            'comment_form': comment_form,
+            'comments': post.comments.all(),
+
         }
 
         return render(request, 'blog/post-detail.html', context)
-        
 
 
 # def post_detaild(req, slug):
